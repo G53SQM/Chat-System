dfa69843d17a1b1dd2e6ba0dbdf9820c30a76d9f
xuanyuan716 <zhanshiwudi010@gmail.com>
2015-01-05 01:40:09 +0000

    Add Model to Client
    Add Model to Client

---
+++ common/MessageType.java
@@ -0,0 +1,13 @@
+package common;
+
+public class MessageType 
+{
+	public static final String MSG_LOGIN_SUCC = "1";
+	public static final String MSG_LOGIN_FAIL = "2";
+	public static final String MSG_PLAIN = "3";
+	public static final String MSG_GET_FND = "4";
+	public static final String MSG_LIST_FND = "5";
+	public static final String MSG_CLIENT_OUT = "6";
+	public static final String MSG_SERVER_OUT = "7";
+	
+}
